19dd338d23cd4dde51fa73601be2f815f4bdcf3b
Alex Beattie <beattieboy@me.com>
2013-07-28 17:25:06 -0400

    Added beginnings of Tilty remote for TiltyWay, fixed DMP6 test file

--- Tilty Software/Arduino/TiltyWay_Remote/TiltyWay_Remote.ino
+++ Tilty Software/Arduino/TiltyWay_Remote/TiltyWay_Remote.ino
@@ -136,6 +136,7 @@ void setup() {
     // (115200 chosen because it is required for Teapot Demo output, but it's
     // really up to you depending on your project)
     Serial.begin(115200);
+    Serial1.begin(230400);
     Wire.begin();
     
     #if defined(__arm__)
@@ -151,6 +152,7 @@ void setup() {
     #endif
     
     while (!Serial); // wait for Leonardo enumeration, others continue immediately
+   
 
     // NOTE: 8MHz or slower host processors, like the Teensy @ 3.3v or Ardunio
     // Pro Mini running at 3.3v, cannot handle this baud rate reliably due to
