3f42c379564077746bd2f57a017c130543161ac2
Alex Beattie <beattieboy@me.com>
2013-08-08 15:00:48 -0400

    Continued GPS testing and added datasheet

--- Tilty Software/Arduino/INO Files/GPS_test/GPS_test.ino
+++ Tilty Software/Arduino/INO Files/GPS_test/GPS_test.ino
@@ -1,27 +1,43 @@
-#define _key 15
-
-int _bauds[] = {9600, 19200, 38400, 57600, 115200, 230400, 460800, 921600};
-
+byte setUBXdataOn[] = {0xB5, 0x62, 0x06, 0x01, 0x03, 0x00, 0xF1, 0x00, 0x01, 0x00, 0x00}; // data to send to turn on UBX location protocol
 
 void setup() {
-  Serial.begin(115200);
-  Serial3.begin(9600);
-  while (!Serial) {}
-  Serial.println("begin...");
-  delay(5);
+	Serial.begin(115200);
+	Serial3.begin(9600);
+	while (!Serial) {}
+	delay(5);
+	Serial.println("begin...");
+	
+	calcChecksum(&setUBXdataOn[2], sizeof(setUBXdataOn) - 4);
+	
+	for (int i = 0; i < sizeof(setUBXdataOn); i++) {
+		Serial.print(setUBXdataOn[i], HEX);
+		Serial.print(", ");
+		Serial3.write(setUBXdataOn[i]);
+	}
+	Serial.println();
 }
 
 void loop() {
- 
-  
-  if (Serial.available()) {
-    while (Serial.available())
-    {  Serial3.print(char(Serial.read()));}
-    Serial3.print("\r\n");
-  }
-  
-  if (Serial3.available()) {
-    while (Serial3.available())
-    {  Serial.print(char(Serial3.read()));}
-  }
+	
+	if (Serial.available()) {
+		while (Serial.available())
+		{	Serial3.write(Serial.read());}
+	}
+	
+	if (Serial3.available()) {
+		while (Serial3.available())
+		{	Serial.print(char(Serial3.read()));}
+	}
 }
+
+void calcChecksum(byte *checksumPayload, byte payloadSize) {
+	byte CK_A = 0, CK_B = 0;
+	for (int i = 0; i < payloadSize ;i++) {
+		CK_A = CK_A + *checksumPayload;
+		CK_B = CK_B + CK_A;
+		checksumPayload++;
+	}
+	*checksumPayload = CK_A;
+	checksumPayload++;
+	*checksumPayload = CK_B;
+}
