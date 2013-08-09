b4ec18f27d0bf14b02621df1a6f753dbb926e127
Alex Beattie <beattieboy@me.com>
2013-08-08 20:57:39 -0400

    Began working on a TiltyIMU library

--- Tilty Software/Arduino/WIP/TiltyIMU_library_testing/TiltyIMU_library_testing.ino
+++ Tilty Software/Arduino/WIP/TiltyIMU_library_testing/TiltyIMU_library_testing.ino
@@ -1,3 +1,16 @@
+#include <TiltyIMU.h>
+
+#include <i2c_t3.h>
+#include <FastServo.h> // Uncomment this line to use Fast Servo library
+//#include <Servo.h> // Uncomment this line to use standard Servo library
+
+#include <I2Cdev.h>
+#include <MPU6050_6Axis_MotionApps20.h> // Comment this line if not using DMP angle measurement
+#include <MPU6050.h>
+#include <MPL3115A2.h>
+
+#include <HMC5883.h>
+
 void setup() {
 	
 }
