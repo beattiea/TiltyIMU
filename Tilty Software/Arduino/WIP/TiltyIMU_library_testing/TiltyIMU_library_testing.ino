a638e84b166a2e9060302dd66009bc4444dee52a
Alex Beattie <beattieboy@me.com>
2013-08-15 16:00:05 -0400

    F'ed up the IMU library trying to get it to work in the TiltyIMU functions

--- Tilty Software/Arduino/WIP/TiltyIMU_library_testing/TiltyIMU_library_testing.ino
+++ Tilty Software/Arduino/WIP/TiltyIMU_library_testing/TiltyIMU_library_testing.ino
@@ -1,4 +1,3 @@
-#include <TiltyIMU.h>
 
 #include <i2c_t3.h>
 #include <FastServo.h> // Uncomment this line to use Fast Servo library
@@ -6,32 +5,35 @@
 
 #include <I2Cdev.h>
 //#include <MPU6050_6Axis_MotionApps20.h> // Comment this line if not using DMP angle measurement
+#define MPU6050_INCLUDE_DMP_MOTIONAPPS20
 #include <MPU6050.h>
 #include <MPL3115A2.h>
 
 #include <HMC5883.h>
 
+#include <TiltyIMU.h>
+
 TiltyIMU tilty;
 
 void setup() {
+	tilty = TiltyIMU();
 	Serial.begin(115200);
 	while (!Serial) {};
 	Serial.println("Beginning");
-	tilty.init();
+	tilty.initializeIMU();
 }
 
 float ypr[3];
 float altitude;
 
 void loop() {
-	tilty.readAltitude(&altitude);
-	Serial.println(altitude);
-	/*
-	tilty.readAngles(ypr);
+	//readAltitude(&altitude);
+	Serial.println("Start Loop");
+	
+	//readAngles(ypr);
 	for (int i = 0; i < 3; i++) {
 		Serial.print(ypr[i]);
 		Serial.print("\t\t");
 	}
-	*/
 	Serial.println();
 }
