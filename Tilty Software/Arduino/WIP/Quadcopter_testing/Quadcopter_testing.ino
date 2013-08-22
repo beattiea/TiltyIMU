022ac66a786429216479f24162ee82dca4897a2e
Alex Beattie <beattieboy@me.com>
2013-08-22 11:27:33 -0400

    Trying Quadcopter test with rate control PID

--- Tilty Software/Arduino/WIP/Quadcopter_testing/Quadcopter_testing.ino
+++ Tilty Software/Arduino/WIP/Quadcopter_testing/Quadcopter_testing.ino
@@ -8,9 +8,11 @@
 #include "Servo.h"
 #include "RCsignal.h"
 
-#define kP 1
-#define kI 0.01
-#define kD 0.2
+float kP = 0.4;
+float kI = 0.0;
+float kD = 0.45;
+
+float iMax = 8;
 
 TiltyIMU tiltyIMU;
 
@@ -27,8 +29,15 @@ float ypr[3];
 float pitchValue;
 float rollValue;
 
-float pitchOffset = 3.08;
-float rollOffset = .85;
+float pitchOffset = 2.74;
+float rollOffset = 0.22;
+
+float d_pitchValue = 0;
+float d_rollValue = 0;
+
+int d_throttle = 0;
+
+float voltage;
 
 volatile int throttle;
 RCsignal throttleRC(21, updateThrottle);
@@ -55,28 +64,30 @@ void setup(){
   ESC3.writeMicroseconds(1000);
   ESC4.writeMicroseconds(1000);
   
-  pitchPID = PID(&ypr[1], &pitchValue, kP, kI, kD, REVERSE);
-  rollPID = PID(&ypr[3], &rollValue, kP, kI, kD, REVERSE);
+  //pitchPID = PID(&ypr[1], &pitchValue, kP, kI, kD, REVERSE);
+  //rollPID = PID(&ypr[2], &rollValue, kP, kI, kD, REVERSE);
   
-  pitchPID.setLimits(-20, 20);
-  rollPID.setLimits(-20, 20);
-  pitchPID.setILimits(-7.5, 7.5);
-  rollPID.setILimits(-7.5, 7.5);
+  pitchPID = PID(&ypr[1], &d_pitchValue, kP, kI, kD, REVERSE);
+  rollPID = PID(&ypr[2], &d_rollValue, kP, kI, kD, REVERSE);
+  
+  //pitchPID.setLimits(-20, 20);
+  //rollPID.setLimits(-20, 20);
+  pitchPID.setILimits(-iMax, iMax);
+  rollPID.setILimits(-iMax, iMax);
   
   for(int i = 0; i < 1000; i++){
 	 tiltyIMU.updateIMU();
 	 digitalWrite(13, LOW);
 	 Serial.println("Zeroing...");
 	 if (i % 100 == 0) {digitalWrite(13, !digitalRead(13));}
+	voltage = (analogRead(14) / 1024.0) * 51.8294;
+	Serial.println(voltage);
   }
   tiltyIMU.readAngles(ypr);
   
-  //pitchOffset = ypr[1];
-  //rollOffset = ypr[2];
-  
-  digitalWrite(13, HIGH);
-   while(throttle > 1150){};
-
+	digitalWrite(13, HIGH);
+	while(throttle > 1150){};
+	d_throttle = throttle;
 }
 
 void updateThrottle() {
@@ -84,35 +95,61 @@ void updateThrottle() {
   
 }
 
-void loop(){
-  
-  //Serial.println(ypr[1]);
-  //Serial.println(ypr[2]);
-  
+void loop() {
+	d_pitchValue = ypr[1];
+	d_rollValue = ypr[2];
+	
   tiltyIMU.updateIMU();
   tiltyIMU.readAngles(ypr);
   
   ypr[1] -= pitchOffset;
   ypr[2] -= rollOffset;
   
-  Serial.println(throttle);
+  d_pitchValue -= ypr[1];
+  d_rollValue -= ypr[2];
   
-  if((throttle > 1200 && throttle != 0 && throttle < 2000) && (abs(ypr[1]) < 15 && abs(ypr[2]) < 15)) //check to make sure that we have a "real" throttle value, and that we're not changing at too much of an angle
-  { 
-  pitchPID.update();
-  rollPID.update();
-  	
-  ESC1.writeMicroseconds(throttle + pitchValue + rollValue);
-  ESC2.writeMicroseconds(throttle + pitchValue - rollValue);
-  ESC3.writeMicroseconds(throttle - pitchValue - rollValue);
-  ESC4.writeMicroseconds(throttle - pitchValue + rollValue);
-  }
-  else{
-  ESC1.writeMicroseconds(1000);
-  ESC2.writeMicroseconds(1000);
-  ESC3.writeMicroseconds(1000);
-  ESC4.writeMicroseconds(1000);
-  }
+  voltage = (analogRead(14) / 1024.0) * 51.8294;
+  
+  //Serial.println(throttle);
+  
+	  if((throttle > 1200 && throttle != 0 && throttle < 2000) && (abs(ypr[1]) < 25 && abs(ypr[2]) < 25)) //check to make sure that we have a "real" throttle value, and that we're not changing at too much of an angle
+	  { 
+		  pitchPID.update();
+		  rollPID.update();
+		  
+		  Serial.print(ypr[1]);
+		  Serial.print(", ");
+		  Serial.print(pitchPID.Pvalue);
+		  Serial.print(", ");
+		  Serial.print(pitchPID.Ivalue);
+		  Serial.print(", ");
+		  Serial.print(pitchPID.Dvalue);
+		  Serial.print(", ");
+		  Serial.print(ypr[2]);
+		  Serial.print(", ");
+		  Serial.print(rollPID.Pvalue);
+		  Serial.print(", ");
+		  Serial.print(rollPID.Ivalue);
+		  Serial.print(", ");
+		  Serial.print(rollPID.Dvalue);
+		  Serial.println();
+		  
+		  pitchValue *= voltage / 12.3;
+		  rollValue *= voltage / 12.3;
+		  	
+		  ESC1.writeMicroseconds(throttle + pitchValue + rollValue);
+		  ESC2.writeMicroseconds(throttle + pitchValue - rollValue);
+		  ESC3.writeMicroseconds(throttle - pitchValue - rollValue);
+		  ESC4.writeMicroseconds(throttle - pitchValue + rollValue);
+	  }
+	  else{
+		  ESC1.writeMicroseconds(1000);
+		  ESC2.writeMicroseconds(1000);
+		  ESC3.writeMicroseconds(1000);
+		  ESC4.writeMicroseconds(1000);
+		  pitchPID.reset();
+		  rollPID.reset();
+	  }
 }
 
 
