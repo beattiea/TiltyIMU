885b664b0ea51b05db42ed8bfcbedc89c8722bc3
Alex Beattie <beattieboy@me.com>
2013-10-06 15:33:21 -0400

    Working R/C signal input test code, and added Atmega328 datasheet to R/C Adapter board files

--- Tilty Software/Arduino/Experiments/RC_Input_2/RC_Input_2.ino
+++ Tilty Software/Arduino/Experiments/RC_Input_2/RC_Input_2.ino
@@ -1,6 +1,6 @@
 #include "Servo.h"
 
-byte inputs[6] = {13, 12, 11, 10, 9, 8};// Input pins
+byte inputs[4] = {9, 8, 5, 6};// Input pins
 byte order[sizeof(inputs)];// Order of data input
 unsigned long last_read = 0;// Time of last input reading
 
@@ -14,19 +14,31 @@ uint16_t channel_max[sizeof(order)];
 uint16_t channel_value[sizeof(order)];
 
 Servo servo;
+#define SERVO_PIN 4
 
 void setup() {
 	Serial.begin(115200);
+	servo.attach(SERVO_PIN);
+	
+	/*
+	while (true) {
+		uint16_t count = TCNT1;
+		delayMicroseconds(1);
+		count = TCNT1 - count;
+		Serial.println(count);
+		delay(500);
+	}
+	*/
+	
 	
 	syncStart(); // Finds gap between transmission of values
 	getOrder(); // finds input order to determine channel number to input pin map
 	index = 0;
-	servo.attach(2);
 }
 
 void loop() {
 	if (digitalRead(order[index])) {
-		channel_start[index] = micros();
+		channel_start[index] = TCNT1;//micros();
 		
 		if (index != 0) {
 			channel_stop[index - 1] = channel_start[index];
@@ -36,13 +48,14 @@ void loop() {
 		
 		if (index == sizeof(order)) {
 			while (digitalRead(order[index - 1])) {}
-			channel_stop[index - 1] = micros();
+			channel_stop[index - 1] = TCNT1;//micros();
 			index = 0;
 			
 			for (int i = 0; i < sizeof(order); i++) {
 				channel_value[i] = channel_stop[i] - channel_start[i];
 				if (channel_value[i] < channel_min[i] || channel_min[i] == 0) { channel_min[i] = channel_value[i];}
-				//if (channel_value[i] > channel_max[i] || channel_max[i] == 0) { channel_max[i] = channel_value[i];}
+				if (channel_value[i] > channel_max[i] || channel_max[i] == 0) { channel_max[i] = channel_value[i];}
+				/*
 				if (!Serial.available()) {
 					Serial.print("Channel ");
 					Serial.print(i + 1);
@@ -59,8 +72,11 @@ void loop() {
 					Serial.print(channel_value[i] - channel_min[i]);
 					Serial.print(", ");
 				}
+				*/
 			}
-			servo.writeMicroseconds(channel_value[5] - channel_min[5] + 1080);
+			if (channel_start[0] < channel_stop[0]) {
+				servo.writeMicroseconds((channel_value[0] - channel_min[0]) / 2 + 1000);
+			}
 			Serial.println();
 		}
 	}
@@ -89,8 +105,6 @@ void getOrder() {
 				if (digitalRead(inputs[i])) {
 					last_read = micros();
 					order[index] = inputs[i];
-					Serial.print("Read ");
-					Serial.println(order[index]);
 					index++;
 				}
 			}
