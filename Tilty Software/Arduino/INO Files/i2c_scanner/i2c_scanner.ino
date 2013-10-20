05f644d8af0cb314428d6068a21e1b65b228efed
Alex Beattie <beattieboy@me.com>
2013-10-19 21:51:02 -0400

    Tiny I2C scanner update

--- Tilty Software/Arduino/INO Files/i2c_scanner/i2c_scanner.ino
+++ Tilty Software/Arduino/INO Files/i2c_scanner/i2c_scanner.ino
@@ -2,6 +2,7 @@
 
 void setup() {
 	Wire.begin(I2C_MASTER, 0x00, I2C_PINS_16_17, I2C_PULLUP_EXT, I2C_RATE_400);
+	while (!Serial) {}
 }
 
 void loop() {
@@ -12,9 +13,9 @@ void loop() {
 			Serial.print(i < 16 ? "I2C Device found at: 0x0" : "I2C Device found at: 0x");
 			Serial.println(i, HEX);
 		}
-	}
-	
+	}	
 	Serial.println();
+	
 	Serial.println("I2C Devices on 3v3 line:");
 	Wire.pinConfigure(I2C_PINS_18_19, I2C_PULLUP_EXT);
 	for (int i = 1; i < 128; i++) {
@@ -26,6 +27,7 @@ void loop() {
 	}
 	
 	while (!Serial.available()) {}
+	
 	Wire.pinConfigure(I2C_PINS_16_17, I2C_PULLUP_EXT);
 	Serial.read();
 	Serial.println();
