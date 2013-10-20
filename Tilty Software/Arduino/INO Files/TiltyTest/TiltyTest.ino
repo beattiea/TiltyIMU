e038c214a03c8d706e2b07f59ea340d268fb8c10
Alex Beattie <beattieboy@me.com>
2013-10-20 14:32:08 -0400

    Updated TiltyTest to check for bluetooth

--- Tilty Software/Arduino/INO Files/TiltyTest/TiltyTest.ino
+++ Tilty Software/Arduino/INO Files/TiltyTest/TiltyTest.ino
@@ -42,14 +42,15 @@ bool compass_avail, imu_avail, alt_avail, flash_avail, bt_avail; // variabless t
 #define VOLTAGE_DIVIDER 0.05061465
 #define SS_PIN 6
 #define BT_COMMAND 15
-#define BT_DEFAULT_BAUD 9600
-#define BT_SET_BAUD 115200
+#define BT_DEFAULT_BAUD 38400
+#define TILTY_DEFAULT_BT_BAUD 115200
 
 void setup()
 {
 	//Open up some serial communications with the computer
 	Serial.begin(115200);
 	pinMode(BT_COMMAND, OUTPUT);
+	digitalWrite(BT_COMMAND, HIGH);
 	
 	flash.begin(SS_PIN, 2);
 	
@@ -83,15 +84,27 @@ void setup()
 	Serial.println(flash_avail ? "OK!" : "NOT OK!");
 	
 	// initialize and check for the bluetooth chip
+	int baud = findBaud();
+	Serial1.begin(baud);//BT_DEFAULT_BAUD);
 	bt_avail = checkBTok();
 	Serial.print("Bluetooth status...\t\t");
-	Serial.println(bt_avail ? "OK!" : "NOT OK!");
+	Serial.println(bt_avail ? "OK!" : "NOT OK!\n");
+	if (bt_avail) {
+		Serial.print("\tBluetooth version: ");
+		getBTversion();
+		//setBTbaud() ? Serial.println("\tBluetooth baud set to default Tilty speed") : Serial.println("\tBluetooth baud not set");
+		Serial.print("\tBluetooth baud set to: ");
+		Serial.println(findBaud());
+		
+		Serial.println();
+	}
 	
 	Serial.println("Enter any character to continue...");
 	
 	while (!Serial.available()) {}
 	while (Serial.available()) { Serial.read();}
 }
+
 
 void loop()
 {
@@ -128,22 +141,117 @@ void loop()
 	if (alt_avail) { while (!altimeter.getDataReady());}
 }
 
+
+
+
 bool checkBTok() {
 	elapsedMillis timer;
 	char ok[4] = {'O', 'K', '\r', '\n'};
 	
-	Serial1.begin(BT_DEFAULT_BAUD);
 	digitalWrite(BT_COMMAND, HIGH);
-	delay(10);
-	Serial1.print("AT\r\n");
+	delayMicroseconds(10);
 	
-	delay(10);
+	Serial1.print("AT\r\n");
+	delayMicroseconds(25);
+	
 	digitalWrite(BT_COMMAND, LOW);
-	delay(25);
+	delayMicroseconds(10);
 	
 	while (Serial1.available() < sizeof(ok) && timer < 250) {}
 	for (int i = 0; i < sizeof(ok); i++) {
 		if (Serial1.read() != ok[i]) {	return false;}
 	}
 	return true;
+}
+
+bool getBTversion() {
+	elapsedMillis timer;
+	char version[9] = {'+', 'V', 'E', 'R', 'S', 'I', 'O', 'N', ':'};
+	
+	Serial1.begin(BT_DEFAULT_BAUD);
+	digitalWrite(BT_COMMAND, HIGH);
+	delayMicroseconds(1000);
+	
+	Serial1.print("AT+VERSION?\r\n");
+	delayMicroseconds(1000);
+	
+	digitalWrite(BT_COMMAND, LOW);
+	delayMicroseconds(1000);
+	
+	while (Serial1.available() < sizeof(version) && timer < 250) {}
+
+	while (Serial1.available()) {
+		for (int i = 0; i < 9; i++) {
+			Serial1.read();
+		}
+		while (Serial1.available()) {
+			char data = Serial1.read();
+			if (data != 'O' && data != 'K' && data != '\n' && data != '\r') {	Serial.print((int)data);}
+			delay(1);
+		}
+	}
+	Serial.println();
+}
+
+bool setBTbaud() {
+	elapsedMillis timer;
+	char ok[4] = {'O', 'K', '\r', '\n'};
+	
+	digitalWrite(BT_COMMAND, HIGH);
+	delayMicroseconds(10);
+	
+	Serial1.print("AT+UART=");
+	Serial1.print(TILTY_DEFAULT_BT_BAUD);
+	Serial1.print(",0,0\r\n");
+	delayMicroseconds(25);
+	
+	digitalWrite(BT_COMMAND, LOW);
+	delayMicroseconds(10);
+
+	while (Serial1.available() < sizeof(ok) && timer < 250) {}
+	for (int i = 0; i < sizeof(ok); i++) {
+		if (Serial1.read() != ok[i]) {	return false;}
+	}
+	
+	digitalWrite(BT_COMMAND, HIGH);
+	delayMicroseconds(10);
+	
+	Serial1.print("AT+RESET\r\n");
+	delayMicroseconds(25);
+	
+	digitalWrite(BT_COMMAND, LOW);
+	delayMicroseconds(10);
+	
+	Serial1.end();
+	delay(25);
+	Serial1.begin(TILTY_DEFAULT_BT_BAUD);
+	delay(1000);
+	
+	int baud = findBaud();
+	if (baud == 115200) {	return true;}
+	else {	return false;}
+}
+
+
+int _bauds[] = {9600, 38400, 57600, 115200, 230400, 460800};
+int findBaud()
+{
+  for (int i = 0; i < sizeof(_bauds) / 4; i++)
+  {
+	digitalWrite(BT_COMMAND, HIGH);
+	Serial1.begin(_bauds[i]);
+	delay(100);
+	Serial1.write("AT\r\n");
+	delay(100);
+	digitalWrite(BT_COMMAND, LOW);
+
+	if (Serial1.available()) 
+	{
+	  while (Serial1.available())//	 Empty the serial buffer. It's enough to know we received data, it doesn't need to be read.
+	  {	Serial1.read();}
+	  return _bauds[i];//  return the correct baud rate
+	}
+	Serial1.end();
+  }
+  return -1;
 }
