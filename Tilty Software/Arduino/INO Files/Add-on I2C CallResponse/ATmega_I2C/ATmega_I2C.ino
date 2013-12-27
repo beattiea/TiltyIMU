259f30a0916be802859b6df899b8d8d4cf3f9e6c
Alex Beattie <beattieboy@me.com>
2013-12-27 12:54:36 -0500

    Added new Tilty Omni design folder

---
+++ Tilty Software/Arduino/INO Files/Add-on I2C CallResponse/ATmega_I2C/ATmega_I2C.ino
@@ -0,0 +1,33 @@
+#include <Wire.h>
+
+volatile int x = 0;
+int y = 10;
+
+void setup() {
+  TWBR = 400000L;
+  pinMode(4, OUTPUT);
+  digitalWrite(4, HIGH);
+  pinMode(9, OUTPUT);
+  digitalWrite(9, LOW);
+  
+  Wire.begin(0x02);
+  Wire.onReceive(receiveEvent);
+  Wire.onRequest(requestEvent);
+}
+
+void loop() {
+  delay(1);
+  //analogWrite(6, map(x, 0, 9, 0, 255));
+}
+
+
+void requestEvent() {
+  byte buff[3] = {byte(x), byte(y), 0x01};
+  Wire.write(buff, 3);
+}
+
+void receiveEvent(int howMany) {
+  if (Wire.available()) {
+    x = Wire.read();
+  }
+}
