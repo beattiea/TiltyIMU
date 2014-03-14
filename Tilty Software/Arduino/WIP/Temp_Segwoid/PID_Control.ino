2144a31c987ae22bc0bbf42392bc11a807890798
Alex Beattie <beattieboy@me.com>
2014-03-13 21:53:27 -0500

    Working rideable code for temp_segwoid

--- Tilty Software/Arduino/WIP/Temp_Segwoid/PID_Control.ino
+++ Tilty Software/Arduino/WIP/Temp_Segwoid/PID_Control.ino
@@ -5,10 +5,14 @@ void updatePID() {
 	P = constrain(kP * ypr[PITCH], MIN_VALUE, MAX_VALUE);
 	D = constrain(kD * (old_ypr[PITCH] - ypr[PITCH]), MIN_VALUE - P, MAX_VALUE - P);
 	I = constrain(I + (kI * ypr[PITCH]), MIN_VALUE - P - D, MAX_VALUE - P - D);	// Calculate I and constrain it so that it does not exceed the max servo value when added to P and Dvalues
+	
+	S = -(analogRead(STEER_SENSE) - STEER_CENTER);
 }
 
 void resetPID() {
 	P = 0;
 	I = 0;
 	D = 0;
+	
+	S = 0;
 }
--- Tilty Software/Arduino/WIP/Temp_Segwoid/Temp_Segwoid.ino
+++ Tilty Software/Arduino/WIP/Temp_Segwoid/Temp_Segwoid.ino
@@ -14,6 +14,11 @@
 
 #include "DualMotorDriver.h"
 
+#define GO_PIN 2
+#define STEER_PIN 3
+#define STEER_SENSE 23
+
+#define STEER_CENTER 373
 
 FreeIMU imu = FreeIMU();	// Simple sensor fusion IMU object
 float ypr[3];				// Simple sensor fusion IMY yaw, pitch, and roll values.
@@ -25,19 +30,21 @@ const short ROLL = 2;		// Roll position in ypr[]
 Servo go_servo;
 Servo turn_servo;
 
-float kP = 0.0;		// Proportional PID gain
-float kI = 0.0;	// Integral PID gain
-float kD = 0.0;		// Derivative PID gain
-float P, I, D;		// PID values
+float kP = 25.0;		// Proportional PID gain
+float kI = 0.25;			// Integral PID gain
+float kD = 7.5;		// Derivative PID gain
+float P, I, D;			// PID values
+
+int S = 255;
 
 const int MAX_STARTING_PITCH = 3; // Maximum pitch before segwoid will begin balancing (must be < this and > -this)
-const int MAX_RUNNING_PITCH = 90; // Maximum pitch before segwoid stops running code and waits to be back in starting pitch range
+const int MAX_RUNNING_PITCH = 20; // Maximum pitch before segwoid stops running code and waits to be back in starting pitch range
 
 void setup() {
 	Serial.begin(115200);
 	
-	go_servo.attach(2);
-	turn_servo.attach(3);
+	go_servo.attach(GO_PIN);
+	turn_servo.attach(STEER_PIN);
 	
 	Wire.begin(I2C_MASTER, 0, I2C_PINS_18_19, I2C_PULLUP_EXT, I2C_RATE_400);
 	
@@ -51,37 +58,55 @@ void setup() {
 		readIMU();
 		delay(10);
 	}
-	
+	/*
 	while (!Serial) { // wait for a serial connection just for debugging purposes
 		readIMU();
 		delay(10);
 	}
+	*/
 	imu.zeroGyro();
+	
+	pinMode(13, OUTPUT);
 }
 
 void loop() {
 	if (abs(ypr[PITCH]) < MAX_STARTING_PITCH) {		// Check to see if segwoid is in a balanced position before starting
+		digitalWrite(13, HIGH);
+		while (abs(S) > 5 && abs(ypr[PITCH]) < MAX_STARTING_PITCH) {
+			readIMU();
+			updatePID();
+			P /= 5;
+			I /= 5;
+			D /= 5;
+			writeServos(P + I + D, 0);
+			printDebugValues();
+			delay(10);
+		}
+		resetPID();
 		while (abs(ypr[PITCH]) < MAX_RUNNING_PITCH) {	// Code to run while segwoid is stable
 			readIMU();
 			updatePID();
 			printDebugValues();
 			
-			writeServos(P + I + D, 0);
+			writeServos(P + I + D, S / 2.5);
+			//writeServos(0, S / 2.5);
 			
-			delay(5);
+			delay(10);
 		}
 	}
 	else resetPID();
 	
+	digitalWrite(13, LOW);
 	readIMU();
 	writeServos(0, 0);
 	printDebugValues();
 	
-	delay(5);
+	delay(10);
 }
 
 void readIMU() {
 	for (int i = 0; i < 3; i++) {	old_ypr[i] = ypr[i];} // Save the old ypr values
+	/*
 	float vals[6];
 	imu.getValues(vals);
 	for (int i = 0; i < 6; i++) {
@@ -90,6 +115,7 @@ void readIMU() {
 		Serial.print(": ");
 		Serial.println(vals[i]);
 	}
+	*/
 	imu.getEuler(ypr); // read new ypr values
 }
 
@@ -100,6 +126,8 @@ void printDebugValues() {
 	Serial.print(ypr[1]);
 	Serial.print(" \tRoll: ");
 	Serial.print(ypr[2]);
+	Serial.print("\t\tSteer: ");
+	Serial.print(analogRead(STEER_SENSE));
 	Serial.print("\t\tP: ");
 	Serial.print(P);
 	Serial.print("\tI: ");
