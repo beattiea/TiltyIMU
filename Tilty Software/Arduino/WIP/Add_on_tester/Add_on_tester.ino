e3ad055a42b8dc953c598d228cf00ac2b7a0c02c
Alex Beattie <beattieboy@me.com>
2014-04-03 21:38:28 -0500

    Little irrelevant tweaks to files

--- Tilty Software/Arduino/WIP/Add_on_tester/Add_on_tester.ino
+++ Tilty Software/Arduino/WIP/Add_on_tester/Add_on_tester.ino
@@ -8,7 +8,7 @@ void setup() {
 	
 	delay(20);
 	
-	Wire.begin(I2C_MASTER, 0, I2C_PINS_16_17, I2C_PULLUP_EXT, I2C_RATE_400);
+	Wire.begin(I2C_MASTER, 0, I2C_PINS_16_17, I2C_PULLUP_EXT, I2C_RATE_200);
 	
 	delay(10);
 }
@@ -72,6 +72,17 @@ void loop() {
 			Serial.read();
 		}
 		
+		else if (Serial.peek() == '{') {
+			setI2Cpins1617();
+			Serial.println("Set I2C line to pins 16 and 17 (5v bus)...\n");
+			Serial.read();
+		}
+		else if (Serial.peek() == '}') {
+			setI2Cpins1819();
+			Serial.println("Set I2C line to pins 18 and 19 (3v3 bus)...\n");
+			Serial.read();
+		}
+		
 		else {	Serial.read();}
 		
 		//Serial.print("Time: ");
@@ -118,8 +129,9 @@ void requestData() {
 		}
 		else {
 			for (int i = 0; i < num; i++) {
-				Serial.print("Received value: ");
+				Serial.print("Received value: 0x");
 				char data = Wire.read();
+				data < 0x10 ? Serial.print("0") : Serial.print(char(27));
 				Serial.print(data, HEX);
 				Serial.print("\t(");
 				Serial.print(data, DEC);
@@ -166,4 +178,25 @@ void get4Bytes() {
 	Serial.println(int32_union.int32);
 	Serial.print("Parsed float value: ");
 	Serial.println(int32_union.flt, 5);
+}
+
+void setI2Cpins1819()
+{
+    CORE_PIN18_CONFIG = (PORT_PCR_MUX(2)|PORT_PCR_ODE|PORT_PCR_SRE|PORT_PCR_DSE);
+    CORE_PIN19_CONFIG = (PORT_PCR_MUX(2)|PORT_PCR_ODE|PORT_PCR_SRE|PORT_PCR_DSE);
+    *portModeRegister(16) = 0; // input
+    CORE_PIN16_CONFIG = PORT_PCR_MUX(1);
+    *portModeRegister(17) = 0; // input
+    CORE_PIN17_CONFIG = PORT_PCR_MUX(1);
+}
+
+// Sets the I2C bus pins on the Teensy to 16 and 17
+void setI2Cpins1617()
+{
+    CORE_PIN16_CONFIG = (PORT_PCR_MUX(2)|PORT_PCR_ODE|PORT_PCR_SRE|PORT_PCR_DSE);
+    CORE_PIN17_CONFIG = (PORT_PCR_MUX(2)|PORT_PCR_ODE|PORT_PCR_SRE|PORT_PCR_DSE);
+    *portModeRegister(18) = 0; // input
+    CORE_PIN18_CONFIG = PORT_PCR_MUX(1);
+    *portModeRegister(19) = 0; // input
+    CORE_PIN19_CONFIG = PORT_PCR_MUX(1);
 }
--- Tilty Software/Arduino/WIP/Temp_Segwoid/Temp_Segwoid.ino
+++ Tilty Software/Arduino/WIP/Temp_Segwoid/Temp_Segwoid.ino
@@ -15,6 +15,9 @@ MPU6050 mpu;
 
 #include "FastServo.h"
 
+#include <TFT.h>
+TFT tft = TFT(10, 9, 8);
+
 #define GO_PIN 2
 #define STEER_PIN 3
 #define STEER_SENSE 23
@@ -41,7 +44,25 @@ int S = 255;
 const int MAX_STARTING_PITCH = 3; // Maximum pitch before segwoid will begin balancing (must be < this and > -this)
 const int MAX_RUNNING_PITCH = 20; // Maximum pitch before segwoid stops running code and waits to be back in starting pitch range
 
+long start;
+
 void setup() {
+    tft.begin();
+    //tft.setTextSize(5);
+	SPI0_CTAR0 &= ~((3 << 16) | 15);
+	SPI0_CTAR0 |= SPI_CTAR_DBR;
+    tft.background(0, 0, 0);
+    tft.setTextSize(2);
+	tft.stroke(ST7735_CYAN);
+	tft.text("Pitch:", 0, 0);
+	tft.text("Roll :", 0, 20);
+	tft.stroke(ST7735_MAGENTA);
+	tft.text("P    :", 0, 40);
+	tft.text("I    :", 0, 60);
+	tft.text("D    :", 0, 80);
+	tft.stroke(ST7735_YELLOW);
+	tft.text("Steer:", 0, 100);
+    
 	Serial.begin(115200);
 	
 	go_servo.attach(GO_PIN);
@@ -66,10 +87,11 @@ void setup() {
 		delay(10);
 	}
 	*/
-	pinMode(13, OUTPUT);
-	digitalWrite(13, HIGH);
+	//pinMode(13, OUTPUT);
+	//digitalWrite(13, HIGH);
 	
 	short count;
+/*
 	while (true) {
 		float std_dev[255];
 		for (int i = 0; i < 255; i++) {
@@ -89,14 +111,17 @@ void setup() {
 		}
 		else {	count = 0;}
 	}
-	
+	*/
 	imu.zeroGyro();
 }
 
 void loop() {
+	start = micros();
+	
 	if (abs(ypr[PITCH]) < MAX_STARTING_PITCH) {		// Check to see if segwoid is in a balanced position before starting
-		digitalWrite(13, HIGH);
+		//digitalWrite(13, HIGH);
 		while (abs(S) > 5 && abs(ypr[PITCH]) < MAX_STARTING_PITCH) {
+			start = micros();
 			readIMU();
 			updatePID();
 			P /= 5;
@@ -104,10 +129,11 @@ void loop() {
 			D /= 5;
 			writeServos(P + I + D, 0);
 			printDebugValues();
-			delay(10);
+			delayMicroseconds(10000 > (micros() - start) ? 10000 - (micros() - start) : 0);
 		}
 		resetPID();
 		while (abs(ypr[PITCH]) < MAX_RUNNING_PITCH) {	// Code to run while segwoid is stable
+			start = micros();
 			readIMU();
 			updatePID();
 			printDebugValues();
@@ -115,17 +141,17 @@ void loop() {
 			writeServos(P + I + D, S / 2.5);
 			//writeServos(0, S / 2.5);
 			
-			//delay(10);
+			delayMicroseconds(10000 > (micros() - start) ? 10000 - (micros() - start) : 0);
 		}
 	}
 	else resetPID();
 	
-	digitalWrite(13, LOW);
+	//digitalWrite(13, LOW);
 	readIMU();
 	writeServos(0, 0);
 	printDebugValues();
-	
-	delay(10);
+
+	delayMicroseconds(10000 > (micros() - start) ? 10000 - (micros() - start) : 0);
 }
 
 void readIMU() {
@@ -133,7 +159,86 @@ void readIMU() {
 	imu.getEuler(ypr); // read new ypr values
 }
 
+
+char arr[6][5];
+int counter;
 void printDebugValues() {
+	if (counter % 5 == 0) {
+		tft.stroke(ST7735_BLACK);
+		tft.text(arr[0], 75, 0);
+		tft.text(arr[1], 75, 20);
+		
+	    String pit = String(int(ypr[1]));
+		String rol = String(int(ypr[2]));
+	    pit.toCharArray(arr[0], sizeof(arr[0]));
+		rol.toCharArray(arr[1], sizeof(arr[1]));
+		
+		int colr;
+		if (abs(ypr[PITCH]) < MAX_STARTING_PITCH) colr = ST7735_GREEN;
+		else if (abs(ypr[PITCH]) >= MAX_STARTING_PITCH && abs(ypr[PITCH]) < MAX_RUNNING_PITCH) colr = ST7735_CYAN;
+		else colr = ST7735_BLUE;
+		
+		tft.stroke(colr);
+    	tft.text(arr[0], 75, 0);
+		tft.text(arr[1], 75, 20);
+	}
+	else if (counter % 5 == 1) {
+		tft.stroke(ST7735_BLACK);
+		tft.text(arr[2], 75, 40);
+		
+	    String p_string = String(int(P));
+	    p_string.toCharArray(arr[2], sizeof(arr[2]));
+		
+		int colr;
+		if (abs(P) < 75) colr = ST7735_GREEN;
+		else if (abs(P) >= 75 && abs(P) < 150) colr = ST7735_CYAN;
+		else colr = ST7735_BLUE;
+		
+		tft.stroke(colr);
+		tft.text(arr[2], 75, 40);
+	}
+	else if (counter % 5 == 2) {
+		tft.stroke(ST7735_BLACK);
+		tft.text(arr[3], 75, 60);
+		
+		String i_string = String(int(I));
+		i_string.toCharArray(arr[3], sizeof(arr[3]));
+		
+		int colr;
+		if (abs(I) < 250) colr = ST7735_GREEN;
+		else if (abs(I) >= 250 && abs(I) < 500) colr = ST7735_CYAN;
+		else colr = ST7735_BLUE;
+		
+		tft.stroke(colr);
+		tft.text(arr[3], 75, 60);
+	}
+	else if (counter % 5 == 3) {
+		tft.stroke(ST7735_BLACK);
+		tft.text(arr[4], 75, 80);
+
+	    String d_string = String(int(D));
+	    d_string.toCharArray(arr[4], sizeof(arr[4]));
+		
+		int colr;
+		if (abs(D) < 5) colr = ST7735_GREEN;
+		else if (abs(D) >= 5 && abs(D) < 10) colr = ST7735_CYAN;
+		else colr = ST7735_BLUE;
+		
+		tft.stroke(colr);
+		tft.text(arr[4], 75, 80);
+	}
+	else if (counter % 5 == 4) {
+		tft.stroke(ST7735_BLACK);
+		tft.text(arr[5], 75, 100);
+
+	    String steer_string = String(int(S / 2.5));
+	    steer_string.toCharArray(arr[5], sizeof(arr[5]));
+		
+		tft.stroke(ST7735_YELLOW);
+		tft.text(arr[5], 75, 100);
+	}
+	counter++;
+    
 	Serial.print("Yaw: ");
 	Serial.print(ypr[0]);
 	Serial.print(" \tPitch: ");
@@ -152,6 +257,7 @@ void printDebugValues() {
 	Serial.print(D);
 	
 	Serial.println();
+	Serial.println(micros() - start);
 }
 
 float standardDev(float *vals, size_t size) {
